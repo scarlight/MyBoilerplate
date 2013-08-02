132735af262a56e089c2ff0fc3109877f45bc291
scarlight <cool64ash@gmail.com>
2013-08-02 18:01:10 +0800

    First Commit
    Completed first rough boilerplate. May need to improve further upon
    completing other new website projects

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+var cs = jQuery.noConflict();
+
+cs(document).ready(function(){
+
+  // code goes here when document is ready
+
+  cs(window).load(function(){
+    
+    // code goes here when window has finish loading
+
+  });
+
+});
