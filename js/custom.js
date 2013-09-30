4cc7a73ce75d70575b69136598555430364cfcad
ScarLight <cool64ash@gmail.com>
2013-09-30 13:59:02 +0800

    Added user agent detection script for IE 10 + css

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,8 @@ var cs = jQuery.noConflict();
 
 cs(document).ready(function(){
 
+    var doc = document.documentElement;
+    doc.setAttribute('data-useragent', navigator.userAgent);
     // code goes here when document is ready
 
     cs(window).load(function(){
