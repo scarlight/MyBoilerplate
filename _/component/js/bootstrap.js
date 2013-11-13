38fc46322248f215ecfa51cab6c09833dc7d6ac7
ScarLight <cool64ash@gmail.com>
2013-11-14 00:35:48 +0800

    New file and removed jquery source mapping file

---
+++ _/component/js/bootstrap.js
@@ -0,0 +1,13 @@
+//@prepros-append jquery-1.10.2.js
+//@prepros-append transition.js
+//@prepros-append alert.js
+//@prepros-append button.js
+//@prepros-append carousel.js
+//@prepros-append collapse.js
+//@prepros-append dropdown.js
+//@prepros-append modal.js
+//@prepros-append tooltip.js
+//@prepros-append popover.js
+//@prepros-append scrollspy.js
+//@prepros-append tab.js
+//@prepros-append affix.js
