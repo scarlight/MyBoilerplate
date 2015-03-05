8fdcf23cc2c83fe572d881d65311767b2acd2482
ScarLight <cool64ash@gmail.com>
2015-03-06 06:03:48 +0800

    Updated with neater comments

--- grunt/less.js
+++ grunt/less.js
@@ -3,11 +3,12 @@ module.exports = function (grunt, options) {
     'use strict';
     grunt.util.linefeed = '\n'; // Force use of Unix newlines. Copied unashamedly from Bootstrap Gruntfile.js
 
-    /*
-        removed newer:{option:{override:...}}} (https://github.com/tschaub/grunt-newer/pull/35)
-        wanted less to re-compile when import file is newer, but it compiles everything and takes up to 3 sec to complete before a livereload
-    */
-    var less = {
+/* ┌──────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
+   │    ─ removed newer:{option:{override:...}}} (https://github.com/tschaub/grunt-newer/pull/35)                     │
+   │   ┌─ wanted less to re-compile when import file is newer, but it compiles everything and takes up to 3 sec       │
+   │   └─ to complete before a livereload                                                                             │
+   └──────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
+*/  var less = {
 
         build: require('./targets/less/build.js')(grunt, options),
 
